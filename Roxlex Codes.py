efc3369dc1bfd315b7f37dbf8a8e64a37bccc955
alexandriley <36522243+alexandriley@users.noreply.github.com>
2018-02-15 22:18:33 -0800

    Add files via upload

---
+++ Roxlex Codes.py
@@ -0,0 +1,52 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> """
+"""
+"""
+Rolex_pt1
+
+Description:
+"""
+import tsk
+import pygame
+pygame.init()
+import random
+window = pygame.display.set_mode([1018,573])
+drawing = True
+back = tsk.Sprite("Fair.jpg", 0, 0)
+dart = tsk.Sprite("Dart.png",500,500)
+
+c = pygame.time.Clock()
+dart_flying = False
+current_why = 0
+why = 0 
+strength = 0
+dart.scale = 1.0
+mouse = True
+while drawing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+        if event.type == pygame.MOUSEBUTTONDOWN and mouse:
+            dart_flying = True
+            mouse = False 
+            why = pygame.mouse.get_pos()
+            why[1]
+            
+            current_why = why[1]
+    if mouse:
+        x, y = pygame.mouse.get_pos()
+        dart.center = x, y
+        strength = why 
+        
+    if dart.center_y <= 500 and dart_flying == False:
+        dart.center_y = 500
+    if dart_flying == True:
+        current_why /= 600
+        
+        dart.scale -= current_why
+        dart.center_y -= 20
+    c.tick(30)
+    back.draw()
+    dart.draw()
+    pygame.display.flip()
